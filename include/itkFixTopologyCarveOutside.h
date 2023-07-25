edba7c564412a6010186a489927d466cce9c7bf7
Bryn Lloyd <lloyd@itis.swiss>
2023-07-25 09:29:22 +0200

    refactor FixTopologyCarveOutside, so code can be shared

--- include/itkFixTopologyCarveOutside.h
+++ include/itkFixTopologyCarveOutside.h
@@ -19,10 +19,7 @@
 #ifndef itkFixTopologyCarveOutside_h
 #define itkFixTopologyCarveOutside_h
 
-#include "itkImageToImageFilter.h"
-#include "itkProgressAccumulator.h"
-
-#include <vector>
+#include "itkFixTopologyBase.h"
 
 namespace itk
 {
@@ -44,7 +41,7 @@ namespace itk
  * \ingroup TopologyControl
  */
 template <class TInputImage, class TOutputImage, class TMaskImage = itk::Image<unsigned char, 3>>
-class ITK_TEMPLATE_EXPORT FixTopologyCarveOutside : public ImageToImageFilter<TInputImage, TOutputImage>
+class ITK_TEMPLATE_EXPORT FixTopologyCarveOutside : public FixTopologyBase<TInputImage, TOutputImage, TMaskImage>
 {
 public:
   /** Extract dimension from input and output image. */
@@ -55,7 +52,7 @@ public:
 
   /** Standard class typedefs. */
   using Self = FixTopologyCarveOutside;
-  using Superclass = ImageToImageFilter<TInputImage, TOutputImage>;
+  using Superclass = FixTopologyBase<TInputImage, TOutputImage, TMaskImage>;
   using Pointer = SmartPointer<Self>;
   using ConstPointer = SmartPointer<const Self>;
 
@@ -63,7 +60,7 @@ public:
   itkNewMacro(Self);
 
   /** Run-time type information (and related methods). */
-  itkTypeMacro(FixTopologyCarveOutside, ImageToImageFilter);
+  itkTypeMacro(FixTopologyCarveOutside, FixTopologyBase);
 
   /** Type for input image. */
   using InputImageType = TInputImage;
@@ -74,89 +71,27 @@ public:
   /** Type for mask image  */
   using MaskImageType = TMaskImage;
 
-  /** Type for the pixel type of the input image. */
-  using InputImagePixelType = typename InputImageType::PixelType;
-
-  /** Type for the pixel type of the input image. */
-  using OutputImagePixelType = typename OutputImageType::PixelType;
-
   /** Pointer Type for input image. */
   using InputImagePointer = typename InputImageType::ConstPointer;
 
   /** Pointer Type for the output image. */
   using OutputImagePointer = typename OutputImageType::Pointer;
 
-  /** Pointer Type for the mask image. */
-  using MaskImageTypePointer = typename MaskImageType::Pointer;
-
-  /** Optional mask (if none is provided the input mask is dilated by 'Radius') */
-  void
-  SetMaskImage(const TMaskImage * mask);
-  const TMaskImage *
-  GetMaskImage() const;
-
-  itkSetMacro(Radius, SizeValueType);
-  itkGetConstMacro(Radius, SizeValueType);
-
-  itkSetMacro(InsideValue, InputImagePixelType);
-  itkGetConstMacro(InsideValue, InputImagePixelType);
-
-#ifdef ITK_USE_CONCEPT_CHECKING
-  /** Begin concept checking */
-  itkConceptMacro(SameDimensionCheck, (Concept::SameDimension<InputImageDimension, 3>));
-  itkConceptMacro(SameTypeCheck, (Concept::SameType<InputImagePixelType, OutputImagePixelType>));
-  itkConceptMacro(InputAdditiveOperatorsCheck, (Concept::AdditiveOperators<InputImagePixelType>));
-  itkConceptMacro(InputConvertibleToIntCheck, (Concept::Convertible<InputImagePixelType, int>));
-  itkConceptMacro(IntConvertibleToInputCheck, (Concept::Convertible<int, InputImagePixelType>));
-  itkConceptMacro(InputIntComparableCheck, (Concept::Comparable<InputImagePixelType, int>));
-  /** End concept checking */
-#endif
-
 protected:
-  FixTopologyCarveOutside();
+  FixTopologyCarveOutside() = default;
   ~FixTopologyCarveOutside() override = default;
-  void
-  PrintSelf(std::ostream & os, Indent indent) const override;
+
+  MaskImageType *
+  CreateDefaultMask(ProgressAccumulator * progress) override;
 
   void
-  GenerateData() override;
+  ComputeThinImage(ProgressAccumulator * progress) override;
 
-  void
-  PrepareData(ProgressAccumulator * progress);
-
-  void
-  ComputeThinImage(ProgressAccumulator * progress);
-
-  std::vector<typename InputImageType::OffsetType>
-  GetNeighborOffsets()
-  {
-    // 18-connectivity
-    using OffsetType = typename InputImageType::OffsetType;
-    return { OffsetType{ -1, 0, 0 }, OffsetType{ 1, 0, 0 }, OffsetType{ 0, -1, 0 },  OffsetType{ 0, 1, 0 },
-             OffsetType{ 0, 0, -1 }, OffsetType{ 0, 0, 1 }, OffsetType{ -1, -1, 0 }, OffsetType{ 1, -1, 0 },
-             OffsetType{ -1, 1, 0 }, OffsetType{ 1, 1, 0 }, OffsetType{ 0, -1, -1 }, OffsetType{ 0, 1, -1 },
-             OffsetType{ 0, -1, 1 }, OffsetType{ 0, 1, 1 }, OffsetType{ -1, 0, -1 }, OffsetType{ 1, 0, -1 },
-             OffsetType{ -1, 0, 1 }, OffsetType{ 1, 0, 1 } };
-  }
+  using ePixelState = typename Superclass::ePixelState;
 
 private:
   ITK_DISALLOW_COPY_AND_ASSIGN(FixTopologyCarveOutside);
 
-  enum ePixelState : OutputImagePixelType
-  {
-    kBackground = 0,
-    kHardForeground,
-    kDilated,
-    kVisited
-  };
-
-  MaskImageTypePointer m_PaddedOutput;
-
-  using RealImageType = itk::Image<float, 3>;
-  RealImageType::Pointer m_DistanceMap;
-
-  SizeValueType       m_Radius = 1;
-  InputImagePixelType m_InsideValue = 1;
 }; // end of FixTopologyCarveOutside class
 
 } // end namespace itk
